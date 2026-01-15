941a53bc063dac4d4ddb339627c36fc259f93142
Edson Carlos <edsoncarlos@ymail.com>
2026-01-14 23:12:41 -0300

    FEAT: Create Order Service

---
+++ src/main/java/com/inovationtech/example_kafka/service/OrderService.java
@@ -0,0 +1,27 @@
+package com.inovationtech.example_kafka.service;
+
+import java.util.Random;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import com.inovationtech.example_kafka.record.OrderRecord;
+
+@Service
+public class OrderService {
+  
+  private final KafkaTemplate<String, OrderRecord> orderKafkaTemplate;
+  private final Random random = new Random();
+
+  public OrderService(KafkaTemplate<String, OrderRecord> orderKafkaTemplate) {
+    this.orderKafkaTemplate = orderKafkaTemplate;
+  }
+
+  @SuppressWarnings("null")
+  public void sendMessageOrder(OrderRecord orderRecord) {
+    int partition = random.nextInt(2);
+    System.out.println("Envie uma mensagem para a partição: " + partition);
+    System.err.println("Enviando o order: " + orderRecord);
+    orderKafkaTemplate.send("napoleon-order-processed", partition, null, orderRecord);
+  }
+}
